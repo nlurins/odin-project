c86991159c4b2f6240b3b6ecea20f337a6645f3d
n.lurins <lurins.normunds@gmail.com>
2022-07-21 20:55:20 +0300

    final fix

--- weather/script.js
+++ weather/script.js
@@ -5,7 +5,6 @@ async function getData(city) {
   const response = await query.json();
   const data = await response;
   const weather = data.weather[0].main;
-  console.log(data);
   document.querySelector("#weather").textContent = data.weather[0].main;
   document.querySelector("#city").textContent = data.name;
   document.querySelector("#temperature").textContent =
@@ -15,24 +14,13 @@ async function getData(city) {
   document.querySelector("#humidity").textContent = data.main.humidity + "%";
   document.querySelector("#wind").textContent =
     Math.floor(data.wind.speed) + "m/s";
-  console.log(window.location.search);
   return data;
 }
 
 getData("london");
-input();
 
-async function getNew() {
-  const str = await window.location.search;
-  const newstr = await str.replace("?search=", "");
-  getData(newstr);
-  console.log(newstr);
-}
-
-function input(){
-  const inputbox = document.querySelector('form');
-  inputbox.addEventListener('submit', e => {
-    e.preventDefault();
-    getNew(inputbox.value);
-  });
-}
+const Element = document.querySelector("form")
+Element.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getData(Element.elements.search.value);
+});
