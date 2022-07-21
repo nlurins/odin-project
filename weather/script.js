1ed11db17e4dc204bbf13dc9b41464473bef84fc
n.lurins <lurins.normunds@gmail.com>
2022-07-21 20:12:09 +0300

    weather app 1.0 TODO fix search

---
+++ weather/script.js
@@ -0,0 +1,28 @@
+async function getData(city) {
+  const query = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9bac5117b64a7764fb9a90466bf4f19f`
+  );
+  const response = await query.json();
+  const data = await response;
+  const weather = data.weather[0].main;
+  console.log(data);
+  document.querySelector("#weather").textContent = data.weather[0].main;
+  document.querySelector("#city").textContent = data.name;
+  document.querySelector("#temperature").textContent =
+    Math.floor(data.main.temp - 273.15) + "°C";
+  document.querySelector("#feels").textContent =
+    Math.floor(data.main.feels_like - 273.15) + "°C";
+  document.querySelector("#humidity").textContent = data.main.humidity + "%";
+  document.querySelector("#wind").textContent =
+    Math.floor(data.wind.speed) + "m/s";
+  console.log(window.location.search);
+  return data;
+}
+
+getData("london");
+
+async function getNew() {
+  const str = await window.location.search;
+  const newstr = await str.replace("?search=", "");
+  await getData(newstr);
+}
